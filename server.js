f1f8a10620e04bccc50ca1636f10464df2378e91
Avrey125 <avreyjbrown@yahoo.com>
2019-09-17 11:34:39 -0700

    locations route added

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+require('dotenv').config();
+
+app.get('/location', (request, response) => {
+  try{
+    let searchQuery = request.query.data;
+    const geoDataResults = require('./data/geo.json');
+  
+
+    const location = new Location(searchQuery, geoDataResults);
+
+    response.status(200).send(location);
+  }
+  catch(err){
+    console.error(err);
+  }
+});
+
+
+function Location(searchQuery, geoDataResults){
+  this.searchQuery = searchQuery;
+  this.formatted_query = geoDataResults.results[0].formatted_address;
+  this.latitude = geoDataResults.results[0].geometry.location.lat;
+  this.longitude = geoDataResults.results[0].geometry.location.lng;
+}
+
+app.use('*', (request, response) => {
+  response.status(404).send('huh?');
+})
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
