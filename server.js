e7578dfa8fff1a96f725df5161e08501e2ae6829
Avrey125 <avreyjbrown@yahoo.com>
2019-09-19 09:27:06 -0700

    lab-07

--- server.js
+++ server.js
@@ -1,137 +1,131 @@
-'use strict'
+
+'use strict';
 
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
-
-app.use(cors());
-
+const cors = require('cors');
+const PORT = process.env.PORT || 3001;
 require('dotenv').config();
 
-app.get('/location', (request, response) => {
-  try{
-    let searchQuery = request.query.data;
-    const geoDataResults = require('./data/geo.json');
-  
 
-    const location = new Location(searchQuery, geoDataResults);
-    response.status(200).send(location);
+const superagent = require ('superagent');
+
+app.use(express.static('public'));
+
+// Worst Policeman ever (everyone can use)
+app.use(cors());
+
+// routes
+app.get('/location', searchLatToLong);
+app.get('/weather', getWeather);
+app.get('/events', getEvents);
+
+
+// function that gets run when someone visits /location
+function searchLatToLong(request, response) {
+
+  //this is what client enters into search box when searching on the front end
+  //this is the city
+  let searchQuery = request.query.data;
+
+
+  // used Kyungrae's key
+  // the url is the API url
+  let URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${process.env.GEOCODE_API_KEY}` 
+
+  // asking superagent to make an api request to google maps
+  superagent.get(URL)
+    .then(superagentResult => {
+      //if succesfull, we store the correct data in the variables we need 
+      let results = superagentResult.body.results[0];
+      const formatted_address = results.formatted_address;
+      const lat = results.geometry.location.lat;
+      const long = results.geometry.location.lng;
+
+
+     // creating a new location object instance using superagent results
+      const location = new Location(searchQuery, formatted_address, lat, long);
+
+      //send that data to the front end
+      response.status(200).send(location);
+
+    })
+    //if we fail we end up here
+    .catch(error => handleError(error, response)
+    )
+  }
+
+// function gets called when the /weather route gets hit 
+function getWeather(request, response) {
+    //this gets the location object from the request 
+    let locationDataObj = request.query.data;
+
+    //get the lat and long
+    let latitude = locationDataObj.latitude;
+    let longitude = locationDataObj.longitude;
+    
+
+    let URL = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${latitude},${longitude}`
+    
+    
+    superagent.get(URL)
+    .then(data =>{
+      let darkskyDataArr = data.body.daily.data;
+      const dailyArr = darkskyDataArr.map(day=>{
+        return new Weather(day);
+      })
+      response.send(dailyArr);
+    })
+    .catch(error => console.log(error));
 
   }
-  catch(err){
-    response.status(500).send("Sorry, something went wrong")
-    console.error(err);
-  }
-});
 
-app.get('/weather', (request, response) => {
-  try {
-    // const searchQuery = request.query.data;
-    const darkSkyDataResults = require('./data/darksky.json');
-    const weathers = darkSkyDataResults.daily.data.map(el => {
-      const forecast = el.summary;
-      const date = new Date(el.time);
-      
-      let weekday;
-      let month;
 
-      // Get weekday
-      switch (date.getDay()) {
-      case 0:
-        weekday = 'Sun';
-        break;
-      case 1:
-        weekday = 'Mon';
-        break;
-      case 2:
-        weekday = 'Tue';
-        break;
-      case 3:
-        weekday = 'Wed';
-        break;
-      case 4:
-        weekday = 'Thu';
-        break;
-      case 5:
-        weekday = 'Fri';
-        break;
-      case 6:
-        weekday = 'Sat';
-        break;
-      default:
-        break;
-      }
-      // Get month
-      switch (date.getMonth()) {
-      case 0:
-        month = 'Jan';
-        break;
-      case 1:
-        month = 'Feb';
-        break;
-      case 2:
-        month = 'Mar';
-        break;
-      case 3:
-        month = 'Apr';
-        break;
-      case 4:
-        month = 'May';
-        break;
-      case 5:
-        month = 'Jun';
-        break;
-      case 6:
-        month = 'Jul';
-        break;
-      case 7:
-        month = 'Aug';
-        break;
-      case 8:
-        month = 'Sep';
-        break;
-      case 9:
-        month = 'Oct';
-        break;
-      case 10:
-        month = 'Nov';
-        break;
-      case 11:
-        month = 'Dec';
-        break;
-      }
 
-      const time = `${weekday} ${month} ${date.getDate()}, ${date.getFullYear()}`;
+function getEvents(response, request){
+  let locationObj = request.query.data;
+  const url = `https://www.eventbriteapi.com/v3/events/search?location.longitude=${lng}&location.latitude=${lat}&expand=venue&token=${process.env.EVENT_API_KEY}&location.address=${locationObj.formatted_address}`
 
-      return {
-        forecast: forecast,
-        time: time
-      };
-    });
-
-    response.status(200).send(weathers);
-  } catch (err) {
-    console.error(err);
-  }
-});
-
-function Location(searchQuery, geoDataResults){
-  this.searchQuery = searchQuery;
-  this.formatted_query = geoDataResults.results[0].formatted_address;
-  this.latitude = geoDataResults.results[0].geometry.location.lat;
-  this.longitude = geoDataResults.results[0].geometry.location.lng;
+  superagent.get(url)
+  .then(eventBriteData => {
+    const eventBriteInfo = eventBriteData.body.events.map(eventData => {
+      const event = new Event(eventData);
+      return event;
+    })
+    response.send(eventBriteInfo);
+  })
+  .catch(error => handleError(error, response));
 }
 
-function Weather(forecast, time) {
-  this.forecast = forecast;
-  this.time = time;
+
+function Event(eventBriteStuff){
+  this.link = eventBriteStuff.url;
+  this.name = eventBriteStuff.name.text;
+  this.event_date = new Date(eventBriteStuff.start.local).toDateString();
+  this.summary = eventBriteStuff.summary;
 }
 
-app.use('*', (request, response) => {
-  response.status(404).send('huh?');
-})
 
-const PORT = process.env.PORT || 3000;
+function Location(searchQuery, address, lat, long){
+  this.search_query = searchQuery;
+  this.formatted_address = address;
+  this.latitude = lat;
+  this.longitude = long;
+}
+
+function Weather(darkSkyData){
+  this.forecast = darkSkyData.summary;
+  this.time = new Date(darkSkyData.time*1000).toDateString();
+}
+
+// our error handler
+function handleError(error, response){
+  console.error(error);
+  const errorObj = {
+    status: 500,
+    text: 'Sorry, something went wrong'
+  }
+  response.status(500).send(errorObj);
+}
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
